aac90a3fb88d86d55e575129b02f384893c66539
dmevans27 <dmevans2720@gmail.com>
2024-03-12 15:00:41 -0400

    input tip display that sequentially shows tips from list at increments of 20 seconds

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -14,7 +14,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScrenState extends State<HomeScreen> {
+  final List<String> tipTitles=[
+    'Health and Safety: Best Time to Water Your Plants',
+    'Indoor Tip: Light Intake',
+    'Plants in Pots: Check your Containers!',
+    'Plants in Pots: Soil Quality',
+  ];
   
+  final List<String> tips = [
+    'Tip 1: Watering your plants is best to do while the sun is out, so that there is less excess hydration overnight. This protects your plants from funguses and bacteria that grow well in excessive dampness!',
+    'Tip 2: Make sure you\'re aware of how much light your plant needs, as some plants need lots of sun and some prefer partial light. This is especially true for indoor plants, as light can be easily manipulated by where they\'re placed indoors.',
+    'Tip 3: For indoor plants, containers do matter! Some plants will keep growing their roots and have trouble with growth if they are unable to spread their roots in a small pot. As such, it\'s inportant to research which plants need what size containers.',
+    'Tip 4: Soil quality is important. Ensure that the soil you have is filled with nutrients for '
+  ];
+  
+  int currentTipIndex= 0;
+  late Timer timer;
+  
+  void initState(){
+    super.initState();
+  
+
+  timer = Timer.periodic(Duration(seconds: 20), (Timer t){
+    setState((){
+      currentTipIndex = (currentTipIndex +1) % tips.length;
+    });
+  });
+}
+@override 
+void dispose(){
+  super.dispose();
+  timer.cancel();
+}
   
   @override
   Widget build(BuildContext context) =>Scaffold(
@@ -55,29 +86,33 @@ class _HomeScrenState extends State<HomeScreen> {
             child: const Text('I made container but I think they need to be something else' ),
           ),
         ),
+      const Text('Tips for Plant Care! New Tip Every 20 Seconds!'),
          Padding(
-          padding: const EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
           child: Container(
             width: 400,
-            height: 100,
+            height: 200,
             color: Colors.blueGrey,
+            child:  Text(
+              tips[currentTipIndex],
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+      ),
+            )
           ),
-        ),
           Padding(
           padding: const EdgeInsets.all(10),
           child: Container(
             width: 400,
-            height: 250,
+            height: 150,
             color: Colors.blueGrey,
             child: const Text(
-              '-Tips of the Day \n -TODO: \n -WATERING SCHEDULE \n -(possible calender package found) \n -Tips: & getting them to display on the homescreen \n -Input Plants & View Plants (text controller?) \n -Plant Disease Buttons Could look Better \n -Navigation Button Colors were for fun, should remove?, \n -Having Watering Schedule Display on Home Screen \n -LOGIN PAGE',
+              '-Tips of the Day \n -TODO: \n -WATERING SCHEDULE \n -(possible calender package found) \n -Input Plants & View Plants (text controller?)  \n -LOGIN PAGE',
             ),
           ),
-        ),
-        
+          ),
       ],
 
      ),
     );
-  }
-
+}
--- lib/tip_list.dart
+++ lib/tip_list.dart
@@ -1,44 +1,48 @@
-/* import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
 
- abstract class ListItem{
-  Widget buildTitle(BuildContext context);
-
-  Widget buildTip(BuildContext context);
-
-final tips = List<String>(
-  ?HeadingItem('Watering Plants')
-  : MessageItem('', 'You should water your plants every day'),
-  
-  
-);
- }
-
- class HeadingItem implements ListItem{
-  final String heading;
-
-  HeadingItem(this.heading);
+class DailyTips extends StatefulWidget {
+  const DailyTips({super.key});
 
   @override
-  Widget buildTitle(BuildContext context){
-    return Text(
-      heading,
-      style: Theme.of(context).textTheme.headlineLarge,
-    );
+  State<DailyTips> createState() => _DailyTipsState();
+}
+
+class _DailyTipsState extends State<DailyTips> {
+  final List<String> tipTitles=[
+    'Health and Safety: Best Time to Water Your Plants',
+    'Indoor Tip: Light Intake',
+    'Plants in Pots: Check your Containers!',
+    'Plants in Pots: Soil Quality',
+  ];
+  
+  final List<String> tips = [
+    'Tip 1: Watering your plants is best to do while the sun is out, so that there is less excess hydration overnight. This protects your plants from funguses and bacteria that grow well in excessive dampness!',
+    'Tip 2: Make sure you\'re aware of how much light your plant needs, as some plants need lots of sun and some prefer partial light. This is especially true for indoor plants, as light can be easily manipulated by where they\'re placed indoors.',
+    'Tip 3: For indoor plants, containers do matter! Some plants will keep growing their roots and have trouble with growth if they are unable to spread their roots in a small pot. As such, it\'s inportant to research which plants need what size containers.'
+    'Tip 4: Soil quality is important. Ensure that the soil you have is filled with nutrients for '
+  ];
+  
+  int currentTipIndex= 0;
+  late Timer timer;
+  
+  void initState(){
+    super.initState();
+  
+
+  timer = Timer.periodic(Duration(seconds: 30), (Timer t){
+    setState((){
+      currentTipIndex = (currentTipIndex +1) % tips.length;
+    });
+  });
+}
+@override 
+void dispose(){
+  super.dispose();
+  timer.cancel();
+}
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
-
-  @override 
-  Widget buildTip(BuildContext context) => const SizedBox.shrink();
- }
-
- class MessageItem implements ListItem{
-  final String sender;
-  final String body;
-
-  MessageItem(this.sender, this.body);
-
-  @override
-  Widget buildTitle(BuildContext context) => Text(sender);
-
-  @override
-  Widget buildTip(BuildContext context)=> Text(body);
- } */
+}
