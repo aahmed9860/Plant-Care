d9eb51615c00403b76682d72e511a33bc4c2abb3
dmevans27 <dmevans2720@gmail.com>
2024-03-14 22:41:50 -0400

    updating todo

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -107,7 +107,7 @@ void dispose(){
             height: 150,
             color: Colors.blueGrey,
             child: const Text(
-              '-Tips of the Day \n -TODO: \n -WATERING SCHEDULE \n -(possible calender package found) \n -Input Plants & View Plants (text controller?)  \n -LOGIN PAGE',
+              '-TODO: \n -WATERING SCHEDULE -(possible calender package found) \n -Input Plants & View Plants (text controller?)  \n -LOGIN PAGE \n -Updating plant disease \n -adding more tips',
             ),
           ),
           ),
