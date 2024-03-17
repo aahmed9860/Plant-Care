bc7c59e102fb64916b6db287c37cf77d03aaaebe
aahmed9860 <146289577+aahmed9860@users.noreply.github.com>
2024-03-16 20:59:57 -0400

    Update ViewPlants.dart

--- lib/ViewPlants.dart
+++ lib/ViewPlants.dart
@@ -1,21 +1,75 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'Navigation.dart';
+import 'package:flutter/services.dart';
+
 class ViewPlants extends StatefulWidget {
-  const ViewPlants({super.key});
+  const ViewPlants({Key? key}) : super(key: key);
 
   @override
   State<ViewPlants> createState() => _ViewPlantsState();
 }
 
 class _ViewPlantsState extends State<ViewPlants> {
+  late List<String> imageAssetNames;
+
+  @override
+  void initState() {
+    super.initState();
+    loadAssets();
+  }
+
+  Future<void> loadAssets() async {
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final imageAssetNames = manifestMap.keys.where((String key) => key.startsWith('asset/') && key.endsWith('.jpg')).toList();
+    setState(() {
+      this.imageAssetNames = imageAssetNames;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    
+    // ignore: unnecessary_null_comparison
+    if (imageAssetNames == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('View Plants'),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: CircularProgressIndicator(), // Show loading indicator while fetching assets
+        ),
+      );
+    }
+
     return Scaffold(
-      drawer: const Navigation(),
       appBar: AppBar(
         title: Text('View Plants'),
         backgroundColor: Colors.red,
-      )
+      ),
+      body: ListView.builder(
+        itemCount: imageAssetNames.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Column(
+              children: [
+                Image.asset(
+                  imageAssetNames[index],
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 200,
+                ),
+                ListTile(
+                  title: Text('Plant ${index + 1}'),
+                  subtitle: Text('Description of ${imageAssetNames[index]}'),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
